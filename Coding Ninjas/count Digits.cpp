2ce46730ea71ae55cf1e99692fafaa7c1bb12d52
Anukul Chandra <101039186+AnukulPr1me@users.noreply.github.com>
2024-01-09 22:19:59 +0530

    day 3

---
+++ Coding Ninjas/count Digits.cpp
@@ -0,0 +1,15 @@
+int countDigits(int n){
+	// Write your code here.
+	int temp = n;
+	int count = 0;
+	while(n!=0)
+	{
+		int rem = n%10;
+		if(rem!=0 && temp%rem==0)
+		{
+			count++;
+		}
+		n=n/10;
+	}
+	return count;	
+}
